839e56c3fd431194c4489f0fc5cc8a59c72829cc
Marius <49654973+JMLF@users.noreply.github.com>
2022-01-24 16:18:01 +0100

    ajout d'un folder d'assets

--- TF_Tensiomètre/Test_Tensiometre/Test_Tensiometre/main.cpp
+++ TF_Tensiomètre/Test_Tensiometre/Test_Tensiometre/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char* argv[])
 	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0) //verifie que l'init s'est bien passé 
 	{
 		
-		SDL_Window* window = SDL_CreateWindow("Tensiometre", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 731, 1110, SDL_WINDOW_RESIZABLE); //on crée la fenetre qu'on met dans un pointeur 
+		SDL_Window* window = SDL_CreateWindow("Tensiometre", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 729, 1010, SDL_WINDOW_RESIZABLE); //on crée la fenetre qu'on met dans un pointeur 
 		SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
 		
 
@@ -15,13 +15,21 @@ int main(int argc, char* argv[])
 		SDL_Rect m_destinationRectangle;
 		SDL_Texture* texture; //on creer une texture pour l'image 
 		SDL_Surface* image = NULL; //pour l'image, on crer une surface
-		
-		image = SDL_LoadBMP("tensiometre-ecran-lire1.bmp"); //on charge l'image dans la surface (a ne faire qu'une fois l'ors de l'init
+	
+		//chiffre ne marche pas 
+	//	SDL_Texture* texture0; //chiffre 
+	//	SDL_Surface* image0 = NULL; //chiffre
+
+		image = SDL_LoadBMP("assets/tensiometre-ecran-lire1.bmp"); //on charge l'image dans la surface (a ne faire qu'une fois l'ors de l'init
 		if (image == NULL)
 			return -1;
 
 		texture = SDL_CreateTextureFromSurface(renderer, image); 
 		SDL_FreeSurface(image);
+
+	//	image = SDL_LoadBMP("assets/FullChiffre.bmp"); //chiffre
+	//	if (image == NULL)//
+	//		return -1;//
 
 		SDL_QueryTexture(texture, NULL, NULL, &m_sourceRectangle.w, &m_sourceRectangle.h);
 
@@ -30,12 +38,18 @@ int main(int argc, char* argv[])
 		m_destinationRectangle.w = m_sourceRectangle.w;
 		m_destinationRectangle.h = m_sourceRectangle.h;
 
+	//	texture0 = SDL_CreateTextureFromSurface(renderer, image0);//chiffre
+	//	SDL_FreeSurface(image0);//chiffre
+	//	SDL_QueryTexture(texture0, NULL, NULL, &m_sourceRectangle.w, &m_sourceRectangle.h);//chiffre
+
+
 		Uint8 a = 255;
 		SDL_GetRenderDrawColor(renderer,0,&a,0,&a);
 
 		SDL_RenderClear(renderer);
 		
 		SDL_RenderCopy(renderer, texture, &m_sourceRectangle, &m_destinationRectangle);
+	//	SDL_RenderCopy(renderer, texture0, &m_sourceRectangle, &m_destinationRectangle);//chiffre
 
 		SDL_RenderPresent(renderer);
 
