cab39fd173af3f38a8feafaaeaa82d931f7e16d2
Marius <49654973+JMLF@users.noreply.github.com>
2022-01-24 09:37:20 +0100

    Création première fenetre SDL2

--- TF_Tensiomètre/Test_Tensiometre/Test_Tensiometre/main.cpp
+++ TF_Tensiomètre/Test_Tensiometre/Test_Tensiometre/main.cpp
@@ -2,19 +2,24 @@
 
 int main(int argc, char* argv[])
 {
-	SDL_Init(SDL_INIT_EVERYTHING);
-	SDL_Window* window = SDL_CreateWindow("Tensiometre", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
-	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+	
+	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0) //verifie que l'init s'est bien passé 
+	{
+		SDL_Window* window = SDL_CreateWindow("Tensiometre", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN); //on crée la fenetre qu'on met dans un pointeur 
+		SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
 
 	
-	SDL_GetRenderDrawColor(renderer,0,0,0,0);
+		SDL_GetRenderDrawColor(renderer,0,0,0,0);
 
-	SDL_RenderClear(renderer);
+		SDL_RenderClear(renderer);
 
-	SDL_RenderPresent(renderer);
+		SDL_RenderPresent(renderer);
 
-	SDL_Delay(3000);
+		SDL_Delay(10000);
 
+		SDL_Quit();
+		return 0;
+	}
 	
-	return 0;
+	return -1;
 }
