6d1d09e4f8afe451c5acb141fcdbdca9f09ba084
Marius <49654973+JMLF@users.noreply.github.com>
2022-02-04 11:02:13 +0100

    attention a la difference entre sdl et sdl2

--- TF_Tensiomètre/Test_Tensiometre/Test_Tensiometre/main.cpp
+++ TF_Tensiomètre/Test_Tensiometre/Test_Tensiometre/main.cpp
@@ -1,3 +1,11 @@
+//https://pub.phyks.me/sdz/sdz/ile-mapping.html
+//utiliser une tiled map pour afficher les constantes 
+//https://openclassrooms.com/forum/sujet/sdl2-et-cours-sur-le-til-mapping
+//https://www.youtube.com/watch?v=FQOiFUl93lI
+//attention a la difference entre sdl et sdl2 
+//
+
+
 #include "SDL.h"
 //#include "../../../../Tensiom-tre-de-formation-Sapeurs-Pompiers-de-l-Indre/SDL2_VS_DEVKIT/include/"
 
